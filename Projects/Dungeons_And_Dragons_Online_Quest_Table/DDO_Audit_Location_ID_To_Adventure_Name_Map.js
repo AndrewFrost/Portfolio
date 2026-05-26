5812e986cf1a63643d68a55f43b32fe9f928bf47
Andrew Frost <andrew.frost@g.austincc.edu>
2026-05-25 19:31:50 -0500

    Updated the DDO Quest Table to a newer version

--- Projects/Dungeons_And_Dragons_Online_Quest_Table/DDO_Audit_Location_ID_To_Adventure_Name_Map.js
+++ Projects/Dungeons_And_Dragons_Online_Quest_Table/DDO_Audit_Location_ID_To_Adventure_Name_Map.js
@@ -1,4 +1,5 @@
-let DDO_Audit_Location_ID_To_Quest_Name_Map = new Map([
+//A mapping of DDO Audit location IDs that have at least one adventure associated with them. Use the map_Array_String generated in adventure_Name_To_DDO_Audit_Location_ID_Map.js to build this list
+let DDO_Audit_Location_ID_To_Adventure_Name_Map = new Map([
 [1879058768, "The Collaborator"],
 [1879059853, "Redemption"],
 [1879058789, "Sacrifices"],
